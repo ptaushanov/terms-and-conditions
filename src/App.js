603f2829c053e61a1c2e6ef11ec498d4f81a3f96
ptaushanov <pepi15032000@gmail.com>
2021-10-06 10:20:16 +0300

    refactor: optimized code for dx

--- src/App.js
+++ src/App.js
@@ -6,24 +6,15 @@ function App() {
   const [terms, setTerms] = useState("");
 
   useEffect(()=>{
-    fetch("https://jaspervdj.be/lorem-markdownum/markdown.txt")
-    .then(response => {
-      if(response.status === 200){
-        return response.text();
-      }
-      throw new Error("Failed to fetch resource!")
-    })
-    .then(data => {
+    (async() => {
+      const response = await fetch("https://jaspervdj.be/lorem-markdownum/markdown.txt");
+      const data = await response.text();
       setTerms(data)
-    })
+    })();
   },[])
 
   return (
-    <div className="App">
-      <div class="container is-fullhd">
-          <Document title={"Terms and Conditions"} content={terms} />
-      </div>
-    </div>
+    <Document title={"Terms and Conditions"} content={terms} />
   );
 }
 
